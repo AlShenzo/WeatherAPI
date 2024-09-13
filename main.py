b4a57ca6f8ab0386a40c18fa9394e1378ce27fed
alans <alanshenjc@gmail.com>
2024-09-13 19:50:35 +0300

    Show station table on the homepage

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import pandas as pd
 app = Flask(__name__)
 
 
+stations = pd.read_csv('data_small/stations.txt', skiprows = 17)
+stations = stations[['STAID', 'STANAME                                 ']]
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', data=stations.to_html())
 # we import render_template and put in tutorial.html in a folder
 # folder is called templates
 # idea of this to allow website parameter/home to take to home page
@@ -15,8 +17,8 @@ def home():
 #<> denote that user can enter value for those
 def about(station,date):
     filename = "data_small/TG_STAID" + str(station).zfill(6) +".txt"
-    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
     # example for str(99).zefill(6)= 000099, 6 digits
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
     temperature = df.loc[df['    DATE'] ==date]['   TG'].squeeze()/10
     return {'station' :station,
         'date': date,
