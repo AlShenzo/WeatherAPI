11953e8565280828307c946f2f2faf9b8c55f423
alans <alanshenjc@gmail.com>
2024-09-13 20:10:42 +0300

    add URL endppoint for yearly and all data

--- main.py
+++ main.py
@@ -25,6 +25,23 @@ def about(station,date):
         'temperature' : temperature}
 
 
+@app.route('/api/v1/<station>')
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    result = df.to_dict(orient='records') # convert to dictionary
+    return result
+
+
+@app.route('/api/v1/yearly/<station>/<year>')
+def yearly(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df['    DATE'] = df['    DATE'].astype(str)
+    result = df[df['    DATE'].str.startswith(str(year))].to_dict(orient = 'records')
+    return result
+
+
 if __name__ =='__main__':
 # this is a condition where we only run our website
 # when this script is executed directly
